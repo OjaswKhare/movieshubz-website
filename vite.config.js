b1fb33914a87bb2c6e11ea98f47e84c02f1125e3
OjaswKhare <96970990+OjaswKhare@users.noreply.github.com>
2025-03-27 14:52:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/movieshubz-website/' // Must match your repo name exactly
+  base: '/movieshubz-website/', // Must match your EXACT repo name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Organized asset structure
+  }
 })
